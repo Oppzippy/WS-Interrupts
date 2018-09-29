b024c16055fbb359a4aaec4aa39df959cd336517
Kyle Scheuing <oppzippy@gmail.com>
2018-09-29 19:52:44 -0400

    Renamed file

---
+++ ParseInput.lua
@@ -0,0 +1,12 @@
+local function LoadString(str)
+	local priorities = { strsplit(";", str) }
+	
+	local priorityList = {}
+	
+	for i, priority in pairs(priority) do
+		local order = { strsplit(">,", priority) }
+		table.insert(priorityList, order)
+	end
+	
+	return priorityList
+end
--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@ Get-Content -Path .\IdTables.lua > build.lua
 Get-Content -Path .\Util.lua >> build.lua
 Get-Content -Path .\CooldownWatcher.lua >> build.lua
 Get-Content -Path .\InterruptPriority.lua >> build.lua
+Get-Content -Path .\ParseInput.lua >> build.lua
 Get-Content -Path .\Main.lua >> build.lua
